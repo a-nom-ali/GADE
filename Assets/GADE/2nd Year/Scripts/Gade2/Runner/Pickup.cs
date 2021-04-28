1b02dc7848525bc1ac5fa90fa092d9bd815916ba
Nielo Wait <nielowait@gmail.com>
2021-04-28 23:07:15 +0200

    Additional Assets

---
+++ Assets/GADE/2nd Year/Scripts/Gade2/Runner/Pickup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PickupType
+{
+    None,
+    Candy,
+    Bubbles,
+    Formula
+}
+public class Pickup : MonoBehaviour
+{
+    [SerializeField] private Transform m_Effect;
+    public virtual PickupType PickupType => PickupType.None;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        var player = other.GetComponent<Player>();
+        player.Pickup(this);
+
+        if (m_Effect != default)
+        {
+            Instantiate(m_Effect, transform.position, Quaternion.identity);
+        }
+        
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/GADE/2nd Year/Scripts/Gade2/Runner/Pickup/CandyPickup.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandyPickup : Pickup
+{
+    [SerializeField] private int m_Count = 1;
+
+    public override PickupType PickupType => PickupType.Candy;
+
+    public int Count => m_Count;
+    
+}
